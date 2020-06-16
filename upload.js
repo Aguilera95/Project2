fc538b52448ddc4691a7f0c6c064f3e46b519aac
Miro <aguilera8756@gmail.com>
2020-06-15 22:41:15 -0600

    update

--- upload.js
+++ upload.js
@@ -2,16 +2,53 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-const express = require("express");
+// const express = require("express");
 
-const app = express();
-const httpServer = http.createServer(app);
+// const app = express();
+// const httpServer = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-httpServer.listen(3000, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// httpServer.listen(3000, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
 
-// put the HTML file containing your form in a directory named "public" (relative to where this script is located)
-app.get("/", express.static(path.join(__dirname, "upload.html")));
+// // put the HTML file containing your form in a directory named "public" (relative to where this script is located)
+// app.get("/", express.static(path.join(__dirname, "upload.html")));
+
+
+function main()
+{
+    var inputFileToLoad = document.createElement("input");
+    inputFileToLoad.type = "file";
+    inputFileToLoad.id = "inputFileToLoad";
+    document.body.appendChild(inputFileToLoad);
+
+    var buttonLoadFile = document.createElement("button");
+    buttonLoadFile.onclick = loadImageFileAsURL;
+    buttonLoadFile.textContent = "Load Selected File";
+    document.body.appendChild(buttonLoadFile);
+}
+
+function loadImageFileAsURL()
+{
+    var filesSelected = document.getElementById("inputFileToLoad").files;
+    if (filesSelected.length > 0)
+    {
+        var fileToLoad = filesSelected[0];
+
+        if (fileToLoad.type.match("image.*"))
+        {
+            var fileReader = new FileReader();
+            fileReader.onload = function(fileLoadedEvent) 
+            {
+                var imageLoaded = document.createElement("img");
+                imageLoaded.src = fileLoadedEvent.target.result;
+                document.body.appendChild(imageLoaded);
+            };
+            fileReader.readAsDataURL(fileToLoad);
+        }
+    }
+}
+
+main();
