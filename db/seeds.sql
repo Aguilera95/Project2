65874d5769eedca95b62c0b9062939a589628e98
SHorne <swhorne@gmail.com>
2020-06-20 00:29:24 -0600

    updating js files.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 use Photogie_db;
 
 INSERT INTO photos (photographer_name, photo_name, photo_description, photo_path, upload_date) VALUES 
-('Sam Horne','The Lonely Road','A dirt road  in Island Park Idaho','','');
+('Sam Horne','The Lonely Road','A dirt road  in Island Park Idaho','/public/uploads/Road.jpg','');
